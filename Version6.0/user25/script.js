7e384ca39dc7a20757e0ec9fb1daf007120f7dc8
ShakoSNOW81 <236312@shakopeeschools.org>
2025-06-02 14:47:59 -0500

    done
    done

---
+++ Version6.0/user25/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#bg-dark').click(function () {
+      $('body').css('background-color', '#222');
+      $('body').css('color', 'white');
+    });
+  
+    $('#bg-light').click(function () {
+      $('body').css('background-color', 'white');
+      $('body').css('color', 'black');
+    });
+  });
+  
